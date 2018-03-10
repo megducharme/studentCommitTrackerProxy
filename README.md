162d976296065b183bc4d23d1cd917660cd30c21
Steve Brownlee <chortlehoort@gmail.com>
2018-03-10 13:24:35 -0600

    makecerts script added

--- README.md
+++ README.md
@@ -1,5 +1,50 @@
-## Student Tracker Proxy Server
-----
-This server is used to call GitHub's API with basic auth to get the most recent commit of the students in my class. GitHub's API allows 60 calls/hour without auth, and 5000 with auth. 
+# Student Tracker Proxy Server
+
+This server is used to call GitHub's API with basic auth to get the most recent commit of the students in my class. GitHub's API allows 60 calls/hour without auth, and 5000 with auth.
 
 To use the server, set up two environment variables with your GitHub username and password, clone and enjoy up to 5000 GitHub API calls!
+
+## Setup
+
+### Create self-signed cert
+
+The string argument is what you intend your local development URL for your project to be (see Hotel section below).
+
+```sh
+./makecerts.sh 'proxy.localhost'
+```
+
+### Using Hotel in Development
+
+[Hotel](https://github.com/typicode/hotel) is a process manager for starting up local servers in development. It supports `https` with some configuration.
+
+### Install Hotel
+
+```sh
+npm install -g hotel
+```
+
+Add proxy service to Hotel
+
+```sh
+hotel add 'NODE_ENVIRONMENT="Development" ghusername="{insert yours here}" ghpassword="{insert yours here}" node server.js' -o spyproxy.log --name spyproxy --port 6060
+```
+
+If you have 2FA enabled, use an OTP token.
+
+```sh
+hotel add 'NODE_ENVIRONMENT="Development" GITHUB_TOKEN="a9f9d937d6a8e9e9d5a8c96c"  node server.js' -o spyproxy.log --name spyproxy --port 6060
+```
+
+Start Hotel
+
+```sh
+hotel start
+```
+
+Visit [http://localhost:2000](http://localhost:2000) and start the `spyproxy` server.
+
+## Verify Proxy Service is Running
+
+
+Visit [http://spyproxy.localhost:6060](http://spyproxy.localhost:6060) you should get a message from the browser about how you are visiting an unsafe site. Allow the browser to continue to the site.
--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 6060
 app.set('port', port)
-const request = require('request');
-const needle = require('needle');
+const request = require('request')
+const needle = require('needle')
 const auth = require('basic-auth')
+const path = require('path')
+const fs = require('fs')
+const https = require('https')
 
-
+// Retrieve relevent environment variables
 const username = process.env.ghusername;
 const password = process.env.ghpassword;
+const environment = process.env.NODE_ENVIRONMENT;
+const otpToken = process.env.GITHUB_TOKEN;
 
 
 // MIDDLEWARE (transform stream)
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -22,25 +27,52 @@ app.use(function(req, res, next) {
 
 
 app.get('/student/commit/*', (req, res) => {
-  
+  const options = {}
   let apiCall = req.url.slice('/student/commit/'.length)
-  
-  var options = {
-    headers: { 
-      'User-Agent': 'megducharme',
+
+  if (otpToken === null) {
+    options.headers = {
+      'User-Agent': 'nashville-software-school',
       'Authorization': 'Basic ' + new Buffer(username + ':' + password).toString('base64')
+    }
+
+  } else {
+    options.headers = {
+      'User-Agent': 'nashville-software-school',
+      'Authorization': `token ${otpToken}`
     }
   }
 
+
   needle("get", apiCall, null, options)
     .then(
-      function(response) {
+      function (response) {
         res.send(response.body)
         console.log(response)
-    });
+      });
 });
 
+if (environment === "Development") {
+  require('ssl-root-cas')
+    .inject()
+    .addFile(path.join(__dirname, 'server', 'my-private-root-ca.cert.pem'));
 
-app.listen(port, () =>
-  console.log(`Listening on port: ${port}`)
-)
+  const options = {
+    key: fs.readFileSync(path.join(__dirname, 'server', 'privkey.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'server', 'fullchain.pem'))
+  };
+
+  const server = https.createServer(options, app).listen(port, function () {
+    const port = server.address().port;
+    console.log('Listening on https://127.0.0.1:' + port);
+    console.log('Listening on https://' + server.address().address + ':' + port);
+    console.log('Listening on https://proxy.localhost:' + port);
+  });
+} else if (environment === "Production") {
+  app.listen(port, () =>
+    console.log(`Student spy proxy is now running
+
+    Listening on https://${server.address().address}:${port}
+    Listening on http://127.0.0.1:${port}`)
+  )
+}
